3e905b03e81086fa01e03abb1d5c269873b59fd1
shenshen1122 <65391047+shenshen1122@users.noreply.github.com>
2021-01-01 17:05:25 +0800

    separeted search user and video to 2 methods

--- src/operation/Search.java
+++ src/operation/Search.java
@@ -28,127 +28,137 @@ public class Search{
         // as long as currentUser has not subscribed to a channel
         // a channel can only be subscribed by 1 user 1 time
         // a channel cannot be subscribed by the SAME USER multiple times !
-        if (c == 'u'){
-            String s;
-            do{
-                System.out.print("Search NAME of Users: ");
-                s = sc.nextLine();
-
-                if (s.isBlank() || s.isEmpty())
-                    System.out.println("search cannot be EMPTY or BLANK!");
-            } while( s.isBlank() || s.isEmpty() );
-
-            do{
-                User[] searchedUsers  = SearchQuery.searchUsers( s , currentUser );
-                printSearchResult( searchedUsers );
-
-                boolean validID = false;
-                System.out.println();
-                System.out.println("Exit subscribing ,Enter \'e\'");
-                System.out.print("To subscribe to a user, Enter userID: ");
-                String op = sc.next();
-                if (op.equals("e"))
-                    return;
-
-                int id;
-                if (isInteger( op )){
-                    id = Integer.parseInt( op );
-                }
-                else{
-                    System.out.println("Enter a valid ID !");
-                    continue;
-                }
-
-                for (User searchedUser : searchedUsers) {
-                    if (id == searchedUser.getUserID()) {
-
-                        // currentUser cannot subscribe to himself/herself
-                        if (currentUser.getUserID() == searchedUser.getUserID()) {
-                            System.out.println("Cannot subscribe to yourself !");
-                            break;
-                        }
-                        // currentUser wants to subscribe to the searchedUser
-                        //     (a)                                  (b)
-                        // but has (a)    subscribed to             (b) before ?
-                        else {
-                            validID = true;
-
-                            // if (a) has subscribed to (b) before
-                            // (a) will now unsubscribes to b
-                            if ( searchedUser.getSubscribed() ) {
-
-                                // delete the record where "currentUser" subscribes to "searchedUser"
-                                SubscriberQuery.delete( currentUser.getUserID() , searchedUser.getUserID() );
-                                // decrease subscribersCount of the searchedUser
-                                Query.decrease("user", "subscribersCount",  1, "userID", searchedUser.getUserID());
-
-                                System.out.println("UNSUBSCRIBED successfully from " + searchedUser.getName());
-                                break;
-                            }
-                            // (a) has not subscribed to (b) before
-                            else {
-                                // insert a new record where "currentUser" subscribes to "searchedUser"
-                                SubscriberQuery.insertNew( currentUser.getUserID() , searchedUser.getUserID() );
-                                // increase subscribersCount of the searchedUser
-                                Query.increase("user", "subscribersCount",  1, "userID", searchedUser.getUserID());
-                                System.out.println("SUBSCRIBED successfully to " + searchedUser.getName());
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (!validID)
-                    System.out.println("Enter a valid ID !");
-            } while (true);
+        if (c == 'u') {
+            searchUser(currentUser);
         }
-
         // does not support playing multiple videos simultaneously
         // because we have to display statistics for each video played
-        else{
-            String s;
-            do{
-                System.out.print("Search TITLE of Videos: ");
-                s = sc.nextLine();
+        else {
+            searchVideo(currentUser);
+        }
+    }
+    public static void searchUser( User currentUser ) throws Exception {
+        Scanner sc = new Scanner(System.in);
 
-                if (s.isBlank() || s.isEmpty())
-                    System.out.println("search cannot be EMPTY or BLANK!");
-            } while( s.isBlank() || s.isEmpty() );
+        String s;
+        do{
+            System.out.print("Search NAME of Users: ");
+            s = sc.nextLine();
 
-            Video[] searchedVideos  = SearchQuery.searchVideos( s );
-            printSearchResult( searchedVideos );
-            do{
-                boolean validID = false;
-                System.out.println();
-                System.out.println("Exit playing ,Enter \'e\'");
-                System.out.print("To play a video, Enter videoID: ");
+            if (s.isBlank() || s.isEmpty())
+                System.out.println("search cannot be EMPTY or BLANK!");
+        } while( s.isBlank() || s.isEmpty() );
 
-                String op = sc.next();
-                if (op.equals("e"))
-                    return;
+        do{
+            User[] searchedUsers  = SearchQuery.searchUsers( s , currentUser );
+            printSearchResult( searchedUsers );
 
-                int id;
-                if (isInteger( op )){
-                    id = Integer.parseInt( op );
-                }
-                else{
-                    System.out.println("Enter a valid ID !");
-                    continue;
-                }
+            boolean validID = false;
+            System.out.println();
+            System.out.println("Exit subscribing ,Enter \'e\'");
+            System.out.print("To subscribe to a user, Enter userID: ");
+            String op = sc.next();
+            if (op.equals("e"))
+                return;
 
-                for (Video searchedVideo : searchedVideos) {
-                    if (id == searchedVideo.getVideoID()) {
+            int id;
+            if (isInteger( op )){
+                id = Integer.parseInt( op );
+            }
+            else{
+                System.out.println("Enter a valid ID !");
+                continue;
+            }
+
+            for (User searchedUser : searchedUsers) {
+                if (id == searchedUser.getUserID()) {
+
+                    // currentUser cannot subscribe to himself/herself
+                    if (currentUser.getUserID() == searchedUser.getUserID()) {
+                        System.out.println("Cannot subscribe to yourself !");
+                        break;
+                    }
+                    // currentUser wants to subscribe to the searchedUser
+                    //     (a)                                  (b)
+                    // but has (a)    subscribed to             (b) before ?
+                    else {
                         validID = true;
 
-                        PlayVideo.withLogin( currentUser, searchedVideo );
-                        // after a video is played, return to MainPage
-                        // does not support playing multiple videos simultaneously
-                        return;
+                        // if (a) has subscribed to (b) before
+                        // (a) will now unsubscribes to b
+                        if ( searchedUser.getSubscribed() ) {
+
+                            // delete the record where "currentUser" subscribes to "searchedUser"
+                            SubscriberQuery.delete( currentUser.getUserID() , searchedUser.getUserID() );
+                            // decrease subscribersCount of the searchedUser
+                            Query.decrease("user", "subscribersCount",  1, "userID", searchedUser.getUserID());
+
+                            System.out.println("UNSUBSCRIBED successfully from " + searchedUser.getName());
+                            break;
+                        }
+                        // (a) has not subscribed to (b) before
+                        else {
+                            // insert a new record where "currentUser" subscribes to "searchedUser"
+                            SubscriberQuery.insertNew( currentUser.getUserID() , searchedUser.getUserID() );
+                            // increase subscribersCount of the searchedUser
+                            Query.increase("user", "subscribersCount",  1, "userID", searchedUser.getUserID());
+                            System.out.println("SUBSCRIBED successfully to " + searchedUser.getName());
+                            break;
+                        }
                     }
                 }
-                if (!validID)
-                    System.out.println("Enter a valid ID !");
-            } while (true);
-        }
+            }
+            if (!validID)
+                System.out.println("Enter a valid ID !");
+        } while (true);
+    }
+    public static void searchVideo( User currentUser ) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        String s;
+        do{
+            System.out.print("Search TITLE of Videos: ");
+            s = sc.nextLine();
+
+            if (s.isBlank() || s.isEmpty())
+                System.out.println("search cannot be EMPTY or BLANK!");
+        } while( s.isBlank() || s.isEmpty() );
+
+        Video[] searchedVideos  = SearchQuery.searchVideos( s );
+        printSearchResult( searchedVideos );
+        do{
+            boolean validID = false;
+            System.out.println();
+            System.out.println("Exit playing ,Enter \'e\'");
+            System.out.print("To play a video, Enter videoID: ");
+
+            String op = sc.next();
+            if (op.equals("e"))
+                return;
+
+            int id;
+            if (isInteger( op )){
+                id = Integer.parseInt( op );
+            }
+            else{
+                System.out.println("Enter a valid ID !");
+                continue;
+            }
+
+            for (Video searchedVideo : searchedVideos) {
+                if (id == searchedVideo.getVideoID()) {
+                    validID = true;
+
+                    PlayVideo.withLogin( currentUser, searchedVideo );
+                    // after a video is played, return to MainPage
+                    // does not support playing multiple videos simultaneously
+                    return;
+                }
+            }
+            if (!validID)
+                System.out.println("Enter a valid ID !");
+        } while (true);
+
     }
     public static void printSearchResult( User[] users){
         // for fun, depends on GUI
